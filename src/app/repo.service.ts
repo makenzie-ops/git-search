064cfba0f5fdad82883b6dbf140def19cca20aea
makenzie-ops <laurenmakenzie0@gmail.com>
2020-04-15 12:16:14 +0300

    updated repo service

--- src/app/repo.service.ts
+++ src/app/repo.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Repo} from './repo'
+import {environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  githubRepo(username):Observable<Repo[]>{
+    return this.http.get<Repo[]>("https://api.github.com/users/" + username + "/repos" + environment.apiKey)
+  }
+
 }
